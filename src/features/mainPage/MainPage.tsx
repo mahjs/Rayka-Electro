6b53813a7f1f8f762b87a18f48102ee1a9d94e29
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-11-19 12:54:48 +0330

    add folder structure and navbar

---
+++ src/features/mainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/navbar/Navbar";
+
+const MainPage = () => {
+  return (
+    <div className="h-[100dvh] md:px-48 px-4 bg-black">
+      <Navbar />
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,9 @@
+import MainPage from "../features/mainPage/MainPage";
+
+export const mainRoutes = [
+  {
+    path: "/",
+    element: <MainPage />,
+    title: "main",
+  },
+];
