e0f3f6ab92a6bd10db7cebcc797a53c564546899
amirkhoda8 <amir.hosein.clan@gmail.com>
2023-11-19 12:57:13 +0330

    fix: mainPage handle

--- src/features/mainPage/MainPage.tsx
+++ src/features/mainPage/MainPage.tsx
@@ -2,8 +2,11 @@ import Navbar from "../../components/navbar/Navbar";
 
 const MainPage = () => {
   return (
-    <div className="h-[100dvh] md:px-48 px-4 bg-black">
-      <Navbar />
+    <div>
+      <div className="h-[100dvh] md:px-48 px-4 bg-black">
+        {" "}
+        <Navbar />
+      </div>
     </div>
   );
 };
