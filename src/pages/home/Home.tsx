261399e9081bac111fcc65e9d0f0806239c474f0
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-11-20 11:12:11 +0400

    refactor buttons

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,10 @@
 import Navbar from "../../components/navbar/Navbar";
 import FirstOfMain from "./FirstOfMain";
 
-const MainPage = () => {
+const Home = () => {
   return (
     <div>
       <div className="h-[100dvh] lg:px-48 md:px-20 px-4 bg-black">
-        {" "}
         <Navbar />
         <FirstOfMain />
       </div>
@@ -13,4 +12,4 @@ const MainPage = () => {
   );
 };
 
-export default MainPage;
+export default Home;
