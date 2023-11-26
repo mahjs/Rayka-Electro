2805ebe412dca9aaf6cb1982ee795020d9b246bb
Sina Sepahvand <sina_sepahvand@email.kntu.ac.ir>
2023-11-26 09:56:52 +0400

    fix: fix login responsive issue

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -6,14 +6,12 @@ import LoginForm from './LoginForm';
 
 const Login: React.FC = () => {
   return (
-    <div className="overflow-x-hidden h-[100dvh] relative bg-[#23153c]">
+    <div className="overflow-x-hidden h-[100dvh] bg-[#23153c]">
       {/* <VantaBackground /> */}
-      <div className="absolute top-0 left-0 right-0">
-        <Navbar />
-      </div>
-      <div className="flex flex-wrap absolute w-full justify-evenly items-center p-4 top-72 md:top-[23.5%]">
+      <Navbar />
+      <div className="flex flex-wrap w-full h-full justify-evenly items-center px-4 md:px-0 mt-3">
         <LoginForm />
-        <img src={LoginImg} alt="login image " className="hidden md:flex w-[35%] h-[35%]" />
+        <img src={LoginImg} alt="login image " className="hidden md:flex w-[50%]" />
       </div>
     </div>
   );
