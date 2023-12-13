f9b997d7bf24096ba2e4447d0c7f2eebec09f1a3
AliNikseresht <alinksht@gmail.com>
2023-12-13 15:48:13 +0330

    fix: fix bg in login & signup with three js

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,13 +4,15 @@ import LoginImg from '../../assets/images/login.webp';
 // import VantaBackground from '../signUp/VantaBackground ';
 import LoginForm from './LoginForm';
 import LazyImage from '../../components/ui/LazyImage';
+import ThreeBackground from './ThreeBackground';
 
 const Login: React.FC = () => {
   return (
-    <div className={`overflow-x-hidden h-[100vh] bg-[#23153c]`}>
+    <div className={`overflow-x-hidden h-[100vh] relative bg-[#23153c]`}>
+      <ThreeBackground />
       {/* <VantaBackground /> */}
       <Navbar swiper={null} currentIndex={0} />
-      <div className="flex flex-wrap w-full h-full justify-evenly items-center px-4 md:px-0 mt-3">
+      <div className="flex absolute flex-wrap top-0 w-full h-full justify-evenly items-center px-4 md:px-0 mt-3">
         <LoginForm />
         <div className="sign-img hidden 2xl:flex w-[50%]">
           <LazyImage src={LoginImg} alt="Login Image" />
